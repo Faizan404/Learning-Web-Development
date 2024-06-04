54602090b506244e85b26903568dd9245cd0b469
Faizan Mustafa <77750924+Faizan404@users.noreply.github.com>
2024-06-04 18:01:30 +0500

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,67 @@
+// What is JavaScript?
+// Highlevel language
+// Multi paradigm
+// Single Threaded
+// Dynamic Programming Language
+
+// console.log("Hello World");
+
+// What are the different data types?
+// String   "faizan343737" 'faizans'
+// Integer +-1, 1.2743876
+// Boolean true false
+// Array [12, 23, "string", true, .....]
+// Object person { name: "Faizan", age: 22}
+
+// What are variables?
+// learningJavaScript
+
+// In dynamic languages
+// let, const, var
+// let num1 = "variable" | 1 | true | []
+// In static languages
+// int num1 = 12;
+
+// let num1 = 12;
+
+// Different ways to print values in JavaScript.
+// console.log()
+// alert()
+// document.write()
+
+// alert("Learning JavaScript");
+// console.log("Faizan Mustafa", 12, true, 2*4);
+// document.write("<h1>Heading</h1>");
+
+// What are JavaScript assignment operators?
+// =
+// ==
+// ===
+// What are arithmetic operators?
+// +, -, *, /, %, **
+
+
+// Task
+// 1. Find the area of a rectangle where the length is 5 and the width is 8.
+let length = 5;
+let width = 8;
+let area = length * width;
+console.log("Area of a rectrangle is : ", area, "sq");
+// 2. Find the area of a triangle where the base is 4 and the height is 3.
+let base = 4;
+let height = 3;
+let areaOfTriangle = (base * height) / 2;
+console.log(areaOfTriangle);
+// 3. Find the area of a circle where the radius is 3.
+let radius = 3;
+const PI = 3.14;
+let areaOfCircle = PI * radius ** 2;
+console.log(areaOfCircle);  
+// 4. Convert temperatures from Celsius to Fahrenheit and Fahrenheit to Celsius
+let celsius = 37;
+let fahrenheit = (9/5 * celsius) + 32;
+console.log(fahrenheit);
+
+let fahrenheit1 = 100;
+let celsius1 =  (fahrenheit - 32) * 5/9;
+console.log(celsius1);
