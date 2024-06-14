b1f0a11c68e441c2289c1ef855067debaf0bc9a8
Faizan Mustafa <77750924+Faizan404@users.noreply.github.com>
2024-06-14 16:54:06 +0500

    todo app
    completed😉✌

--- js/projects/todo/todo.js
+++ js/projects/todo/todo.js
@@ -1,13 +1,13 @@
 // DOM (Document Object Model) Manipulation
 
 // CRUD Create,Read,Update,Delete
-const myTodosList = [];
 const listGroup = document.getElementById("list-group");
+
+let myTodosList = [];
+let index = 1;
 
 function createTodo() {
   const createForm = document.querySelector(".form-create");
-
-  let index = 1;
 
   createForm.addEventListener("submit", function (e) {
     e.preventDefault();
@@ -20,7 +20,7 @@ function createTodo() {
         data-edit=${index}
         class="list-group-item d-flex justify-content-between align-items-center">
             <span>${todoContent}</span>
-            <i class="far fa-trash-alt delete"></i>
+            <i data-del=${index} class="del-icon far fa-trash-alt delete"></i>
     </li>`;
     listGroup.insertAdjacentHTML("afterbegin", listItem);
     // const listItem = document.createElement("li");
@@ -28,6 +28,8 @@ function createTodo() {
     // listGroup.appendChild(listItem);
 
     myTodosList.push({ id: index++, content: todoContent });
+    this[0].value = "";
+    localStorage.setItem("todoList", JSON.stringify(myTodosList));
     console.log(myTodosList);
   });
 }
@@ -35,18 +37,18 @@ function createTodo() {
 function readTodo() {
   listGroup.innerHTML = "";
   let listItem = "";
-  
+
   myTodosList.forEach((todo) => {
     listItem += ` 
     <li
         data-edit=${todo.id}
         class="list-group-item d-flex justify-content-between align-items-center">
             <span>${todo.content}</span>
-            <i class="far fa-trash-alt delete"></i>
+            <i data-del=${todo.id} class="del-icon far fa-trash-alt delete"></i>
     </li>`;
   });
-  
-  listGroup.insertAdjacentHTML("afterend", listItem);
+
+  listGroup.insertAdjacentHTML("afterbegin", listItem);
 }
 
 function updateTodo() {
@@ -58,7 +60,7 @@ function updateTodo() {
     let listItem = e.target.closest(".list-group-item");
     if (!listItem) return;
     const todoId = listItem.dataset.edit;
-    console.log(myTodosList)
+    console.log(myTodosList);
     filteredTodo = myTodosList.filter((todo) => todo.id === +todoId);
 
     editInput.value = filteredTodo[0].content;
@@ -73,13 +75,35 @@ function updateTodo() {
       (todo) => todo.id === filteredTodo[0].id
     );
     myTodosList[checkTodoIndex] = filteredTodo[0];
-
+    this[0].value = "";
     readTodo();
   });
 }
-function deleteTodo() {}
+function deleteTodo() {
+  listGroup.addEventListener("click", function (e) {
+    const todo = e.target.closest(".del-icon");
+    if (!todo) return;
+    const todoId = todo.dataset.del;
+    console.log(todo, todoId)
+    const delTodoIndex = myTodosList.findIndex((todo) => todo.id === +todoId);
+    myTodosList.splice(delTodoIndex, 1);
+    localStorage.setItem("todoList", JSON.stringify(myTodosList));
+    readTodo();
+    console.log(myTodosList);
+  });
+}
+
+function setAppData() {
+  let todoAppData = localStorage.getItem("todoList");
+  if (!todoAppData) return;
+  myTodosList = JSON.parse(todoAppData);
+  readTodo();
+  console.log(myTodosList);
+}
 
 (function init() {
+  setAppData();
   createTodo();
   updateTodo();
+  deleteTodo();
 })();
